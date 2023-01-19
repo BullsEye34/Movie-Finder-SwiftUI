cdc2db33fd1358df18356958390927bf46fa7ce6
P Vamshi Prasad <vamvijkum@gmail.com>
2023-01-19 12:39:33 +0530

    Initial Commit

---
+++ Movie Finder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Movie Finder
+//
+//  Created by P Vamshi Prasad on 19/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Movie Finder/Movie_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_FinderApp.swift
+//  Movie Finder
+//
+//  Created by P Vamshi Prasad on 19/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Movie_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
