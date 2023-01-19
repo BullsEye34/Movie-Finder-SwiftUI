30b72d8b998a98acbde2e619cb42fffb4d7bedba
P Vamshi Prasad <vamvijkum@gmail.com>
2023-01-19 14:00:54 +0530

    Define Utils Class

---
+++ Movie Finder/Utils.swift
@@ -0,0 +1,22 @@
+//
+//  Utils.swift
+//  Movie Finder
+//
+//  Created by P Vamshi Prasad on 19/01/23.
+//
+
+import Foundation
+
+class Utils{
+    static let jsonDecoder: JSONDecoder = {
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        jsonDecoder.dateDecodingStrategy = .formatted(dateFormatter)
+        return jsonDecoder
+    }()
+    
+    static let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-mm-dd"
+        return dateFormatter
+    }()
+}
