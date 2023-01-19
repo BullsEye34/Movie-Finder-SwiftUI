e048e9ef3bb4d13c7b9ef3a9404ee9e7dcde54fe
P Vamshi Prasad <vamvijkum@gmail.com>
2023-01-19 13:44:44 +0530

    Create MovieModel

---
+++ Movie Finder/Models/MovieModel.swift
@@ -0,0 +1,25 @@
+//
+//  MovieModel.swift
+//  Movie Finder
+//
+//  Created by P Vamshi Prasad on 19/01/23.
+//
+
+import Foundation
+
+// MARK: - MovieResponse
+struct MovieResponse: Decodable {
+    let results: [Result]
+}
+
+// MARK: - Result
+struct Result: Decodable {
+    let id: Int
+    let backdropPath: String
+    let title: String
+    let originalTitle, overview, posterPath: String
+    let popularity: Double
+    let releaseDate: String
+    let voteAverage: Double
+    let voteCount: Int
+}
